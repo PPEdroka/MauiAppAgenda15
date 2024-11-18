fbdc1720f3bbc55ad85042c640f84dea16d5072d
Admin <Admin@DESKTOP-2KP0K61>
2024-11-18 05:17:49 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppAgenda15/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppAgenda15
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
